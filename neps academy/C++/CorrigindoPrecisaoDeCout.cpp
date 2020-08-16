29eb572016c2c5e929f05957a2fce4de769ca83e
Danilo Freire <50254168+DaniloVFreire@users.noreply.github.com>
2020-08-16 20:13:16 -0300

    Add files via upload

---
+++ neps academy/C++/CorrigindoPrecisaoDeCout.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  float valor = 2.456345;
+  cout << valor << endl;
+
+  cout.precision(2);
+  //cout.setf(ios::fixed);
+  cout << valor << endl;
+  return 0;
+}
